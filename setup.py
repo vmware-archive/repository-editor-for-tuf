dbecb7ff4d649cfca24730ee2f7129c4ff0c8dac
Jussi Kukkonen <jkukkonen@vmware.com>
2021-07-15 22:34:52 +0300

    Add the initial code
    Signed-off-by: Jussi Kukkonen <jkukkonen@vmware.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+from setuptools import setup
+
+setup(
+    name='tufrepo',
+    version='0.1.0',
+    py_modules=['tufrepo'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tufrepo = tufrepo:cli',
+        ],
+    },
+)
